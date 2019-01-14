a7ac7cde2a67984ccd64c2e9333d1fd2a51ac2c5
Glitch (fcc-filemeta-fx) <none>
2019-01-14 07:52:25 +0000

    🛀📻 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer = require('multer');
+var upload = multer();
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  const resJSON = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size    
+  };
+  res.send(resJSON);
+});
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
